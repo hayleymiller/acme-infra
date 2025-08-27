668d6ae815d3eff73e0c6ac2a9d69c8a6284ff52
hayleymiller <hayley.miller@ibm.com>
2025-08-26 23:00:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "hello" {
   # use your org name here (visible in HCP Terraform org settings)
-  source      = "Hayley-Onboarding-Org/hello/null"
+  source      = "app.terraform.io/Hayley-Onboarding-Org/hello/null"
   version     = "1.0.0"
   cost_center = var.cost_center
 }
