3d44f1f2625d4433947d88e8efe5893cc07e8a64
hayleymiller <hayley.miller@ibm.com>
2025-08-26 23:36:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "hello" {
   source      = "app.terraform.io/Hayley-Onboarding-Org/hello/null"
-  version     = "1.0.0"
+  version     = "1.0.1" #upgrade to non-compliant version
   cost_center = var.cost_center
 }
