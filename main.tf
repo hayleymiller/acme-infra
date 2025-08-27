c2aea1cbed2664cba214491691697c0337355742
hayleymiller <hayley.miller@ibm.com>
2025-08-27 00:34:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ resource "random_pet" "name" {
   prefix = "${var.name_prefix}-${var.environment}"
 }
 
+module "hello" {
+  source      = "app.terraform.io/Hayley-Onboarding-Org/hello/null"
+  version     = "1.0.0"
+
+  name_prefix = var.name_prefix
+  cost_center = var.cost_center
+}
+
+
 # Safe local resource we can "tag" and update repeatedly
 resource "null_resource" "marker" {
   triggers = {
