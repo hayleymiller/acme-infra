c0db82e2088f7fe969f94c510cc94793a1c58c13
hayleymiller <hayley.miller@ibm.com>
2025-08-26 23:57:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 module "hello" {
   source      = "app.terraform.io/Hayley-Onboarding-Org/hello/null"
   version     = "1.0.0" 
+
+# inputs
+  name_prefix = "acme-app"
   cost_center = var.cost_center
+  stamp       = "v1" 
 }
+
