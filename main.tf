9c43cd02e4d070e8b2a5b653b6a55a87943ec34f
hayleymiller <hayley.miller@ibm.com>
2025-08-27 00:21:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,8 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
-    }
-  }
-}
+resource "random_pet" "name" {}
 
-# Generates a harmless name; no cloud resources created
-resource "random_pet" "name" {
-  prefix = var.name_prefix
-}
-
-# The resource our Sentinel policy evaluates
 resource "null_resource" "hello" {
   triggers = {
-    cost_center = var.cost_center   # required by policy
+    cost_center = var.cost_center
     name        = random_pet.name.id
-    bump       = var.bump        # lets us force an update during demo
   }
 }
