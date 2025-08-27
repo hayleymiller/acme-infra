d7bd0290bdda5ddd0a2143ed485046c391977422
hayleymiller <hayley.miller@ibm.com>
2025-08-27 00:25:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
-resource "random_pet" "name" {}
+# acme-infra/main.tf
+data "aws_ami" "al2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter { name = "name"; values = ["amzn2-ami-hvm-*-x86_64-gp2"] }
+}
 
-resource "null_resource" "hello" {
-  triggers = {
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.al2.id
+  instance_type = var.instance_type
+  tags = {
+    Name        = "${var.name_prefix}-web"
     cost_center = var.cost_center
-    name        = random_pet.name.id
   }
 }
