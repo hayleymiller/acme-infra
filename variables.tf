1eb51f9c6bcc964d8e5399e5e38c9cbbfc42e778
hayleymiller <hayley.miller@ibm.com>
2025-08-27 00:30:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "cost_center" {
-  type        = string
-  description = "Required business tag / cost allocation"
-  default     = "" # we'll set it in the workspace variables
-}
+variable "name_prefix"  { type = string, default = "acme" }
+variable "environment"  { type = string, default = "dev" }
+variable "cost_center"  { type = string, default = "ACME-RND" }
+# harmless toggle so you can force a change for live demos
+variable "bump"         { type = string, default = "v1" }
